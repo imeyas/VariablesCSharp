a83cc0fb4d37d98586f2a311712f774f6ab93d71
Ebo Onwunali <ebo@njgeneral.com>
2022-01-07 12:10:32 -0500

    Finished variables CSharp exercise

---
+++ VariablesCSharp.cs
@@ -0,0 +1,10 @@
+Variable Assignment 
+
+string myName = "Ebo Onwunali";
+int myAge = 34;
+char canCallMe = 'E';
+bool iAmAmerican = true;
+double myWeight = 160.5;
+decimal hopefulBitcoinBalance = 45.198m;
+
+Console.WriteLine($"My name is {myName}, currently {myAge} years old. By the way you can also call me {canCallMe}. If you MUST know, lol, my current weight is {myWeight}. While we are at it, my Bitcin balance should be {hopefulBitcoinBalance}");
